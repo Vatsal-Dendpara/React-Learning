6356dbbe7e5c40b71b1423b8635fcdd2119668be
Vatsal-Mtech <vatsal.dendpara@marutitech.com>
2023-05-05 12:52:14 +0530

    added comments and updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # React-Learning
+
+### To learn basics about react, `useState and useEffect` hook go inside the `monsters-rolodex` project folder see the code written inside the `App.js` file.
+
+> this project includes basic functionality like get data from the server (side effects), set the state of components and filteration of the data.
+>
+> to run the project first download the dependencies using `npm i or npm install`.
+>
+> after that to run the project use `yarn start or npm start`.
--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -4,11 +4,12 @@ import SearchBox from "./components/search-box/search-box.component";
 import CardList from "./components/card-list/card-list.component.jsx";
 
 function App() {
+  //state initialization
   const [searchField, setSearchField] = useState("");
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-  console.log("render");
+  // for getting the data from the API
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
@@ -17,13 +18,16 @@ function App() {
       });
   }, []);
 
+  //for filter the data based on the search parameters
   useEffect(() => {
     const newFilteredMonsters = monsters.filter((monster) => {
       return monster.name.toLowerCase().includes(searchField);
     });
+    //set the state of filter data.
     setFilteredMonsters(newFilteredMonsters);
   }, [monsters, searchField]);
 
+  //to set the state of the search field
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLowerCase();
     setSearchField(searchFieldString);
